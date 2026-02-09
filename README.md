0d7a6cdc318b3409d317fdf28e7e1e4f549abba8
Sopaipilla <sopaipillagithub@gmail.com>
2026-02-09 00:07:03 -0300

    Generacion del desafio 2 febrero 2026 G107

---
+++ README.md
@@ -0,0 +1,3 @@
+# Desarrollo desafío 2 de Condiciones
+## Desafío Latam G107
+### Febrero 2026
---
+++ js/condicionales.js
@@ -0,0 +1,27 @@
+// Desafío 1
+
+let objetoSelccionado = false;
+const imagenConBorde = document.getElementById("imagenSeleccionada");
+
+if(objetoSelccionado)
+{
+	// Podemos aplicar css directamente en JS.
+	// Podemos usar parámetros en la propiedad 'border'.
+	imagenConBorde.style.border = "5px solid red";
+	objetoSelccionado != objetoSelccionado;
+}
+elseif(!objetoSelccionado)
+{
+	// Podemos aplicar css directamente en JS.
+	// Podemos usar el parámetro none para eliminar el borde.
+	imagenConBorde.style.border = "none";
+	objetoSelccionado != objetoSelccionado
+}
+
+// Desafío 2
+
+
+
+
+
+
